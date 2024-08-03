46e93a30416f37bf4a22deaa3a115595b861ff6d
Nicola Romano <romano.nicola@gmail.com>
2024-08-03 15:04:27 +0100

    added config file

--- simulation.py
+++ simulation.py
@@ -3,18 +3,21 @@ from random import randint, random
 from scipy.spatial import distance_matrix
 import numpy as np
 from math import pi, cos, sin
+import json
+import os
 
 # Define agent types
 AGENT_TYPES = ["Food", "Prey", "Predator"]
 
+
 class Agent:
     # Note that we put the type hint for the simulation parameter as a string because the Simulation class is defined after the Agent class
-    def __init__(self, type: str, 
-                 x_pos: float, y_pos: float, speed: float, energy: float,                 
+    def __init__(self, type: str,
+                 x_pos: float, y_pos: float, speed: float, energy: float,
                  angle: float, angular_speed: float, simulation: "Simulation"):
         """
         Initializes an agent for the simulation
-        
+
         Parameters
         ----------
         type: str - the type of agent
@@ -39,10 +42,27 @@ class Agent:
         self.angular_speed = angular_speed
         self.energy = energy
         self.alive = True
+        self.age = 0
 
         self.nn = NN.createNeuralNetwork(3, [4, 2], 2, NN.relu, 0.5)
 
-    def __str__(self):
+    def die(self) -> None:
+        """
+        Kills the agent, setting its alive status to False and energy to 0
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+
+        self.alive = False
+        self.energy = 0
+
+    def __str__(self) -> str:
         return f"Agent {self.id} at ({self.x}, {self.y}) with speed {self.speed} and angular speed {self.angular_speed}"
 
 
@@ -61,13 +81,16 @@ class Simulation:
         -------
         None
         """
+        
+        self.read_config()
+        
         self.width = width
         self.height = height
         self.current_epoch = 0
         self.agents = []
         self.agent_ids = {type: 0 for type in AGENT_TYPES}
         self.log_messages = []
-        
+
         self.history = {
             "Epoch": [0],
             "Food": [num_agents[0]],
@@ -90,12 +113,34 @@ class Simulation:
                 if AGENT_TYPES[i] == "Food":
                     energy = 100
                 else:
-                        energy = randint(50, 100)
+                    energy = randint(50, 100)
 
                 self.agents.append(
                     Agent(AGENT_TYPES[i], x_pos, y_pos, speed, energy, angle, angular_speed, self))
-        self.log(f"Started simulation with {num_agents[0]} food, {num_agents[1]} prey, and {num_agents[2]} predators")
+        self.log(
+            f"Started simulation with {num_agents[0]} food, {num_agents[1]} prey, and {num_agents[2]} predators")
 
+    def read_config(self) -> None:
+        """
+        Reads the configuration file for the simulation (config.json)
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        None
+        """
+        
+        assert "config.json" in os.listdir(), "config.json not found"
+        
+        self.config = {}
+        
+        with open("config.json", "r") as f:
+            self.config = json.load(f)        
+        
+        
     def get_next_id(self, type: str) -> int:
         """
         Gets the next id for an agent of the given type
@@ -111,154 +156,178 @@ class Simulation:
         id = self.agent_ids[type]
         self.agent_ids[type] += 1
         return id
-            
-    
+
+    def get_alive_agents(self, include_food=True) -> list[Agent]:
+        """
+        Gets the list of agents that are alive
+
+        Parameters
+        ----------
+        include_food: bool, defaults to True - whether to include food agents in the list
+
+        Returns
+        -------
+        list[Agent] - the list of agents that are alive
+        """
+
+        if include_food:
+            return [agent for agent in self.agents if agent.alive]
+        else:
+            return [agent for agent in self.agents if agent.alive and agent.type != "Food"]
+
     def step(self) -> None:
         """
         Runs a single step of the simulation
-        
+
         Parameters
         ----------
         None
-        
+
         Returns
         -------
         None
         """
-        
+
         x_pos = [agent.x for agent in self.agents]
         y_pos = [agent.y for agent in self.agents]
         pos = np.column_stack((x_pos, y_pos))
         distances = distance_matrix(pos, pos)
-        
+
         # Make the diagonal elements infinity so that the agent doesn't consider itself as the closest agent
         distances[range(len(distances)), range(len(distances))] = float("inf")
-        
+
+        # If there are no agents left, end the simulation
+        if len(self.get_alive_agents(include_food=False)) == 0:            
+            self.log(f"Simulation ended at epoch {self.current_epoch}")
+            return
+
         # Update the position of each agent
-        for ag_id, agent in enumerate(self.agents):
+        for ag_id, agent in enumerate(self.get_alive_agents()):
             if agent.type == "Food":
-                continue # Food doesn't move
-            
+                continue  # Food doesn't move
+
+            agent.age += 1
+
             # Run the neural network to get the speed and angular speed
             # Inputs to the neural network are the x and y positions of the closest agent and the type of the closest agent
             # Outputs are the speed and angular speed
             closest_agent = self.agents[distances[ag_id].argmin()]
-            agent.speed, agent.angular_speed = agent.nn.forward([closest_agent.x, closest_agent.y, AGENT_TYPES.index(closest_agent.type)])
-                        
+            agent.speed, agent.angular_speed = agent.nn.forward(np.array([closest_agent.x, closest_agent.y, AGENT_TYPES.index(closest_agent.type)]))
+
             agent.angle += agent.angular_speed % (2 * pi)
             agent.x += agent.speed * cos(agent.angle)
             agent.y += agent.speed * sin(agent.angle)
             agent.x %= self.width
             agent.y %= self.height
-            
+
             agent.energy -= 1
             if agent.energy <= 0:
                 self.agents.remove(agent)
-            
-        # If health is >80, reproduce. Preys have a higher chance of reproducing        
-        for agent in self.agents:
-            if agent.energy >= 80 and agent.type != "Food":      
-                repro_chance = 0.4 if agent.type == "Prey" else 0.3
+ 
+        for agent in self.get_alive_agents():
+            if agent.energy >= self.config['min_health_for_repro'] and agent.type != "Food":
+                repro_chance = self.config['prey_repro_prob'] if agent.type == "Prey" else self.config['pred_repro_prob']
                 if random() < repro_chance:
                     self.agents.append(
-                        Agent(agent.type, agent.x + randint(-1, 1), agent.y + randint(-1, 1), 
-                            agent.speed + random() * 2.0 - 1.0, 
-                            randint(50, 100), agent.angle + random() * .25 * pi,
-                                agent.angular_speed + random() * pi/6 - pi/12, self))                    
-                    
-                agent.energy -= 10 # Energy cost of reproduction
-                
+                        Agent(agent.type, agent.x + randint(-1, 1), agent.y + randint(-1, 1),
+                              agent.speed + random() * 2.0 - 1.0,
+                              randint(50, 100), agent.angle +
+                              random() * .25 * pi,
+                              agent.angular_speed + random() * pi/6 - pi/12, self))
+
+                agent.energy -= self.config['repro_cost']  # Energy cost of reproduction
+
         # Check if any 2 agents are in the same position
         self.check_collision()
-                            
+
         for a in self.agents[::-1]:
             if a.energy <= 0:
-                self.agents.remove(a)
-        
+                a.die()
+
         # Food respawns randomly with a certain probability
-        if random() < 0.1:
+        if random() < self.config['food_respawn_prob']:
             self.agents.append(
                 Agent("Food", randint(0, self.width), randint(0, self.height), 0, 100, 0, 0, self))
-                            
+
         self.current_epoch += 1
         self.__update_history()
-        if (self.history['Prey'][-1] == 0 and self.history['Predator'][-1] == 0):
-            self.log(f"Simulation ended at epoch {self.current_epoch}")
-            return
 
     def check_collision(self) -> None:
         """
         Checks if any 2 agents are in the same position (or very close) and updates their energy accordingly
-        """        
-        for agent1 in self.agents:
-            for agent2 in self.agents:
+        """
+        for agent1 in self.get_alive_agents():
+            for agent2 in self.get_alive_agents():
                 if agent1 == agent2:
                     continue
-                if agent1.energy <= 0 or agent2.energy <= 0:
-                    continue
+                # if agent1.energy <= 0 or agent2.energy <= 0:
+                #     continue
 
                 if ((agent1.x - agent2.x) ** 2 + (agent1.y - agent2.y) ** 2) < 1:
                     # Both Preys and predators can eat food, but Preys get more energy from it. Predators eat food only if they are at low energy
-                    
+
                     # Prey eats food
-                    if (agent1.type == "Food" and agent2.type == "Prey") or (agent1.type == "Prey" and agent2.type == "Food"):                        
+                    if (agent1.type == "Food" and agent2.type == "Prey") or (agent1.type == "Prey" and agent2.type == "Food"):
                         agent2.energy = min(agent2.energy + 20, 100)
                         if agent1.type == "Food":
-                            agent1.energy = 0                            
+                            agent1.die()
                         else:
-                            agent2.energy = 0
-                            
+                            agent2.die()
+
                     # Predator eats food
                     elif (agent1.type == "Food" and agent2.type == "Predator" and agent2.energy < 25) or (agent1.type == "Predator" and agent2.type == "Food" and agent1.energy < 25):
                         if agent1.type == "Food":
-                            agent1.energy = 0
-                            agent2.energy = min(agent2.energy + 2, 100)                            
+                            agent1.die()
+                            agent2.energy = min(agent2.energy + 2, 100)
                         else:
                             agent1.energy = min(agent1.energy + 2, 100)
-                            agent2.energy = 0                            
+                            agent2.die()
 
-                    # Prey eats predator                            
+                    # Prey eats predator
                     elif (agent1.type == "Prey" and agent2.type == "Predator"):
                         agent2.energy = min(agent2.energy + 10, 100)
-                        agent1.energy = 0                        
+                        agent1.die()
                     elif (agent1.type == "Predator" and agent2.type == "Prey"):
                         agent1.energy = min(agent1.energy + 10, 100)
-                        agent2.energy = 0                        
+                        agent2.die()
                     else:
                         pass
 
     def __update_history(self) -> None:
         """
         Updates the history of the simulation
-        
+
         Parameters
         ----------
         None
-        
+
         Returns
         -------
         None
-        """        
-        
+        """
+
         self.history["Epoch"].append(self.current_epoch)
-        self.history["Food"].append(len([agent for agent in self.agents if agent.type == "Food"]))
-        self.history["Prey"].append(len([agent for agent in self.agents if agent.type == "Prey"]))
-        self.history["Predator"].append(len([agent for agent in self.agents if agent.type == "Predator"]))
+        self.history["Food"].append(
+            len([agent for agent in self.agents if agent.type == "Food"]))
+        self.history["Prey"].append(
+            len([agent for agent in self.agents if agent.type == "Prey"]))
+        self.history["Predator"].append(
+            len([agent for agent in self.agents if agent.type == "Predator"]))
 
     def log(self, msg: str) -> None:
         """
         Adds a message to the log. These are displayed by the app
-        
+
         Parameters
         ----------
         msg: str - the message to log
-        
+
         Returns
         -------
         None
         """
-        
+
         self.log_messages.append(msg)
-        
+
     def __str__(self):
-        return f"Simulation with {len(self.agents)} agents in a {self.width}x{self.height} grid"    
+        return f"Simulation with {len(self.agents)} agents in a {self.width}x{self.height} grid"
